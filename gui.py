04df1a4c7ca183c45b1468bb0f75d476ba9b87ff
LilWeavs <andrewjw827@gmail.com>
2021-12-13 23:13:16 -0600

    added scrollbar and text memory limits

--- gui.py
+++ gui.py
@@ -1,12 +1,11 @@
 from os import error
 from typing import TypeGuard
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSerialPort
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QMenuBar, QPlainTextEdit, QPushButton, QWidget, QTextBrowser, QGridLayout, QLineEdit, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPlainTextEdit, QPushButton, QWidget, QTextBrowser, QGridLayout, QLineEdit, QComboBox
 import sys
 from serial.serialwin32 import Serial
 from serial.tools import list_ports
 from serial.serialutil import SerialException
-import typing
 import serial
 from threading import Thread, current_thread
 from queue import Queue
@@ -15,13 +14,13 @@ from queue import Queue
 class SerialPort(QtCore.QObject):
     port  = 'COM1'
     baudRate = 9600
-    def __init__(self, parent = None) -> None:
+    def __init__(self, q: Queue, parent = None) -> None:
         super().__init__(parent)
-        
+        self.msg = q
     # Explicit Signal
-    dataReady = QtCore.pyqtSignal(str)
+    # dataReady = QtCore.pyqtSignal(str)
 
-    @QtCore.pyqtSlot(str)
+    # @QtCore.pyqtSlot(str)
     def startSerialCommunication(self):
         stop = current_thread()
         with serial.Serial(self.port, self.baudRate, timeout=1) as ser:
@@ -30,7 +29,8 @@ class SerialPort(QtCore.QObject):
             while getattr(stop, "do_run", True):
                 line = ser.readline()
                 line = line.decode('ascii')
-                self.dataReady.emit(line)
+                self.msg.put(line)
+                # self.dataReady.emit(line)
 
 class GUI(QMainWindow):
     """Main Window"""
@@ -50,6 +50,11 @@ class GUI(QMainWindow):
         self.baudRateMenu       = QComboBox()
         self.openSerialButton   = QPushButton('Connect')
         self.comPortMenu        = QComboBox()
+
+        """ASCII Browser Settings And Parameters"""
+        self.serialInputBrowser.setMaximumBlockCount(500) # Limits Text Log to 500 lines of text
+        self.scrollBar = self.serialInputBrowser.verticalScrollBar()
+        self.scrollBar.setValue(self.scrollBar.maximum())
 
         """Layout Widgets In GUI Window"""
         layout = QGridLayout()
@@ -81,62 +86,34 @@ class GUI(QMainWindow):
         self.baudRateMenu.currentTextChanged.connect(self.setBaudRate)
 
         """Serial Object Settings"""
-        # self.device = SerialPort()
-        # self.device.port = 'COM1'
-        # self.device.baudRate = 9600
-        # self.device = serial.Serial()
-        # self.device.port = 'COM1'
-        # self.device.baudrate = 9600
-        # self.device = QtSerialPort.QSerialPort()
-        # self.device.setPortName('COM4')
-        # self.device.setBaudRate(QtSerialPort.QSerialPort.Baud115200)
-        # self.device.readyRead.connect(self.textWindowHandler)
-        # self.connected = False
 
-        # self.dataReady = QtCore.pyqtSignal(str)
-        # self.dataReady.connect(self.textWindowHandler)
     
 
     def sendButtonHandler(self, e):
-        # print(self.serialInputEditor.text())
-
-        # txt = [hex(ord(x)) for x in self.serialInputEditor.text()]
-        # txt = ' '.join(txt)
         self.serialInputBrowser.insertPlainText(self.serialInputEditor.text())
-        # self.serialInputBrowser.append(self.serialInputEditor.text())
-        # self.serialInputEditor.inputPlainText(txt)
         self.serialInputEditor.clear()
 
     def setBaudRate(self, e):
         self.baudrate = int(e)
-        # self.device.setBaudRate(QtSerialPort.QSerialPort.Baud115200)
         print("BaudRate: ", int(e))
 
     def setComPort(self, e):
         self.port = e
-        # self.device.setPortName(e)
         print("Port: ", e)
 
     def openSerialPort(self, e):
-            # self.serialThread = QtCore.QThread()
-            # self.device.moveToThread(self.serialThread)
-            # self.device.dataReady.connect(self.serialThread.quit)
-            # self.serialThread.started.connect(self.device.startSerialCommunication)
-            # self.serialThread.start()
-            # self.connected = True
         if self.openSerialButton.text() == "Connect":
             try:
-                # self.textThread = Thread(target=self.textWindowHandler)
-                # self.textThread.daemon = True
-                self.device = SerialPort()
+                self.device = SerialPort(self.msg)
                 self.device.port = self.port
                 self.device.baudRate = self.baudrate
-                self.device.dataReady.connect(self.textWindowHandler)
-                
-                # self.dataReady.connect(self.textWindowHandler)
-                # self.textThread = QtCore.QThread()
-                # self.textThread.start()
-                # self.textThread.started.connect(self.textWindowHandler)
+                # self.device.dataReady.connect(self.textWindowHandler)
+
+                self.timer = QtCore.QTimer()
+                self.timer.setInterval(1000//30)
+                self.timer.timeout.connect(self.textWindowHandler)
+                self.timer.start()
+
                 self.serialThread = Thread(target=self.device.startSerialCommunication)
                 self.serialThread.daemon = True
 
@@ -150,44 +127,15 @@ class GUI(QMainWindow):
         else:
             self.openSerialButton.setText('Connect')
             self.serialThread.do_run = False
+            self.timer.stop()
             # self.textThread.do_run = False
 
-    def textWindowHandler(self, data):
-        # print(data)
-        self.serialInputBrowser.insertPlainText(data)
-        self.serialInputEditor.verticalScrollBar().maximum()
-        # dataReady = QtCore.pyqtSignal(str)
-        # stop = current_thread()
-        # # while getattr(stop, "do_run", True):
-        # while getattr(stop, "do_run", True):
-        #     if not self.msg.empty():
-        #         line = self.msg.get(0)
-        #         self.msg.task_done()
-        #         self.dataReady.emit(line)
+    def textWindowHandler(self):
 
-
-        #         self.serialInputBrowser.insertPlainText(line)
-        #         print(line)
-        
-                # self.serialInputBrowser.verticalScrollBar().maximum()
-                    # print(line)
-
-    # def startSerialCommunication(self):
-        
-    #     try:
-    #         stop = current_thread()
-    #         self.device.open()
-    #         print('Connected!')
-    #         while getattr(stop, "do_run", True):
-    #             line = self.device.readline()
-    #             s = line.decode('ascii')
-    #             self.msg.put(s)
-
-    #     except SerialException as err:
-    #         print(err)
-
-    #     self.device.close()
-    #     print('Disconnected!')
+        print(self.msg.qsize())
+        for i in range(self.msg.qsize()):
+            self.serialInputBrowser.insertPlainText(self.msg.get())
+        self.scrollBar.setValue(self.scrollBar.maximum())
 
 
 if __name__ == "__main__":
